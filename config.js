336fc1f8455090d38e3ea69f8f97d72440596afa
devilnelu <chutisose@gmail.com>
2024-10-19 17:54:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=PzRBTBLb#bckaFQqZIx0CIVobaKnrXZBA3DBf3QLlkLD89l_3I0Q",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
